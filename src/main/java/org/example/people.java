f018d7386ff685a6bc20e138245246327f40b5c1
data8320-03 <soosumg522@gmail.com>
2024-04-29 12:59:01 +0900

    gop

---
+++ src/main/java/org/example/people.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class people {
+    public static void main(String[] args) {
+        int gop = 1;
+
+        for (int i = 1; i <= 5; i ++) {
+            gop = gop * i;
+        }
+        System.out.println("i의 곱셈은 : " + gop);
+        }
+    }
